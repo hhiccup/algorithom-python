9f1765c36b8c1e4a80c6fd4068e5a64847484415
hhiccup <41107232+hhiccup@users.noreply.github.com>
2020-05-27 21:43:46 +0800

    Update move_zeros.py

--- move_zeros.py
+++ move_zeros.py
@@ -1,3 +1,5 @@
+
+#第一次写的： 不知道属于什么法
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
@@ -9,6 +11,7 @@ class Solution:
                 count0 = count0 + 1 
             elif count0 != 0:
                 nums[i-count0], nums[i] = nums[i], 0
-    return 0
     
     
+    
+#
