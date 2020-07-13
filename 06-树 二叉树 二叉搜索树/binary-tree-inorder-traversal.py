e3645f7ca023c5879d2fc747d0102b5ac8443442
hhiccup <41107232+hhiccup@users.noreply.github.com>
2020-07-13 20:22:07 +0800

    Update binary-tree-inorder-traversal.py

--- 06-树 二叉树 二叉搜索树/binary-tree-inorder-traversal.py
+++ 06-树 二叉树 二叉搜索树/binary-tree-inorder-traversal.py
@@ -44,3 +44,33 @@ class Solution: #test1 题解的迭代法（自己维护一个栈）
         return res
 #层序遍历的话就把stack变成queue，pop就改成pop(0)，因为是先进先出
    
+##################2、binary-tree-preorder-traversal#####################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+'''
+class Solution:# try1 直接递归 O(n) O(h)
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+'''
+
+class Solution:# try2   带标记迭代 5min
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        stack = [(0, root)]
+        while stack:
+            flag, node = stack.pop()
+            if not node: continue
+            if flag == 0:
+                stack.append((0, node.right))
+                stack.append((0, node.left))
+                stack.append((1, node))
+            else:
+                res.append(node.val)
+        return res
+
