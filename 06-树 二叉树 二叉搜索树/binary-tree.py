124dce13ca53e77e9d5041b02593c9396bf5b47d
hhiccup <41107232+hhiccup@users.noreply.github.com>
2020-07-15 16:38:25 +0800

    Update binary-tree.py

--- 06-树 二叉树 二叉搜索树/binary-tree.py
+++ 06-树 二叉树 二叉搜索树/binary-tree.py
@@ -250,3 +250,32 @@ class Codec:
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
+
+
+
+################### 6 最近公共祖先 （中）########################
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution: #test1 看题解后写
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q: #终止条件
+            return root
+        #获取左右子节点信息
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        #回溯
+        if not left: 
+            return right
+        if not right:
+            return left
+        return root
+
+            
+
+########################   ############################
